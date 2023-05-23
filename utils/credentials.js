b98975fab54e3bdf4a5d5c3df0daf0a5dd2f9111
Gnud23 <104758571+gn0ht4n@users.noreply.github.com>
2023-05-23 16:35:26 +0700

    update

---
+++ utils/credentials.js
@@ -0,0 +1,13 @@
+module.exports = {
+    database: {
+        mysql: {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'tdtutube',
+        },
+    },
+    cookie: {
+        secret: 'my_secret@',
+    }
+}
